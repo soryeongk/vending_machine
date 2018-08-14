0b3776d821442bb723fcb4d807fc13463db7a312
soryeongk <rlathfud2@gmail.com>
2018-08-14 16:46:21 +0900

    create what the class is in python

--- vm.py
+++ vm.py
@@ -1,23 +1,21 @@
-change = 0
 
-def init():
-    global change
-    change = 0
 
-def run(raw):
-    global change
+class VendingMachine:
+    def __init__(self):
+        self.change = 0
 
-    tokens = raw.split(' ')
-    cmd, params = tokens[0], tokens[1:]
+    def run(self, raw):
+        tokens = raw.split(' ')
+        cmd, params = tokens[0], tokens[1:]
 
-    if cmd == '잔액':
-        return '잔액은 {}원입니다'.format(change)
-    elif cmd == '동전':
-        coin = params[0]
-        change += int(coin)
-        return coin + '원을 넣었습니다'
-    else:
-        return '알 수 없는 명령입니다'
+        if cmd == '잔액':
+            return '잔액은 {}원입니다'.format(self.change)
+        elif cmd == '동전':
+            coin = params[0]
+            self.change += int(coin)
+            return coin + '원을 넣었습니다'
+        else:
+            return '알 수 없는 명령입니다'
 
 # def vm(vm_dict):
 #     coin = int(input('동전을 넣어주세요: '))
