0a7a4d7a36ffdd92a13b128cc86ccc978ed2ed79
soryeongk <rlathfud2@gmail.com>
2018-08-28 11:33:09 +0900

    우유와 밀크커피를 추가

--- test_vm.py
+++ test_vm.py
@@ -29,6 +29,8 @@ def test_give_me_coffee():
     m.run('동전 500')
     assert '커피가 나왔습니다' == m.run('음료 커피')
     assert '잔액은 350원입니다' == m.run('잔액')
+    m.run('음료 우유')
+    assert '잔액은 150원입니다' == m.run('잔액')
 
 def test_unknown_drink():
     m = VendingMachine()
@@ -40,9 +42,15 @@ def test_unknown_coins():
     m = VendingMachine()
     assert '알 수 없는 동전입니다' == m.run('동전 130')
     assert '잔액은 0원입니다' == m.run('잔액')
+#
+# def test_return_change():
+#     m = VendingMachine()
+#     m.run('동전 500'); m.run('동전 100'); m.run('동전 50')
+#     m.run('동전 10'); m.run('동전 10')
+#     assert '500원 100원 50원 10원 10원 ' == m.run('반환')
 
-def test_return_change():
+def test_milk_coffee():
     m = VendingMachine()
-    m.run('동전 500'); m.run('동전 100'); m.run('동전 50')
-    m.run('동전 10'); m.run('동전 10')
-    assert '500원 100원 50원 10원 10원 ' == m.run('반환')
+    m.run('동전 500')
+    assert '밀크커피가 나왔습니다' == m.run('음료 밀크커피')
+    assert '우유가 나왔습니다' == m.run('음료 우유')
--- vm.py
+++ vm.py
@@ -15,26 +15,35 @@ class VendingMachine:  # 상태와 연산을 정의
             self.change += int(coin)
             return coin + '원을 넣었습니다'
         elif cmd == '음료':
-            price = 150
+            drinks_dict = {
+                '커피' : 150,
+                '우유' : 200,
+                '밀크커피' : 300,
+            }
             drink = params[0]
-            if drink != '커피':
+            if drink not in drinks_dict.keys():
                 return '알 수 없는 음료입니다'
+
+            price = drinks_dict[drink]
             if self.change < price:
                 return '잔액이 부족합니다'
+
             self.change -= price
             return drink + '가 나왔습니다'
-        elif cmd == '반환':
-            c_500 = self.change//500
-            self.change = self.change % 500
-            c_100 = self.change//100
-            self.change = self.change % 100
-            c_50 = self.change//50
-            self.change = self.change % 50
-            c_10 = self.change//10
-            self.change = self.change % 10
-            return '500원 '*c_500 + '100원 '*c_100 + '50원 '*c_50 + '10원 '*c_10
+
         else:
             return '알 수 없는 명령입니다'
+
+        # elif cmd == '반환':
+        #     c_500 = self.change//500
+        #     self.change = self.change % 500
+        #     c_100 = self.change//100
+        #     self.change = self.change % 100
+        #     c_50 = self.change//50
+        #     self.change = self.change % 50
+        #     c_10 = self.change//10
+        #     self.change = self.change % 10
+        #     return '500원 '*c_500 + '100원 '*c_100 + '50원 '*c_50 + '10원 '*c_10
 
 # def vm(vm_dict):
 #     coin = int(input('동전을 넣어주세요: '))
